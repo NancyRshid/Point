8db680bf53cdd63b8feb46dc4cc9ad4f4cebee9d
NancyRashid <116031655+NancyRshid@users.noreply.github.com>
2022-11-18 23:43:56 +0200

    Add files via upload

---
+++ Point3D.java
@@ -0,0 +1,52 @@
+
+package test.java;
+
+public class Point3D extends Point2D {
+  private float z = 0.0f;  
+
+    public Point3D() {
+        super();
+    }
+    
+
+    public Point3D(float x, float y , float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    
+    public void setXYZ(float x, float y , float z){
+        super.setX(x);
+        super.setY(y);
+        this.z = z;
+    }
+    
+    public float[] getXYZ(){
+       float[] arr1 = new float[3];  
+       arr1[0] = 5;
+       arr1[1] = 3;
+       arr1[2] = 4;
+       return arr1;
+        
+    } 
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
